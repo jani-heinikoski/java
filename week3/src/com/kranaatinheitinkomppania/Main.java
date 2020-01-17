91dab624c2ba9b368d28089584f27d197615a321
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-01-17 16:38:43 +0200

    week_3_added
    -

---
+++ week3/src/com/kranaatinheitinkomppania/Main.java
@@ -0,0 +1,10 @@
+package com.kranaatinheitinkomppania;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Kaisu " + i);
+        }
+    }
+}
