6e48059558d6e20f961645e06c655a53647ec2fb
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-01-18 17:33:11 +0200

    ver_4.2.2
    viopesta läpi

--- week4/src/com/kranaatinheitinkomppania/Main.java
+++ week4/src/com/kranaatinheitinkomppania/Main.java
@@ -2,7 +2,7 @@
 Author: Jani Heinikoski | 0541122
 Date: 18.1.2020
 Header: CT60A2411_07.01.2020 | Olio-ohjelmointi | WEEK 3
-Version: 4.1.0
+Version: 4.2.2
  */
 package com.kranaatinheitinkomppania;
 
@@ -10,6 +10,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    ReadAndWriteIO rwIO = new ReadAndWriteIO();
-	    rwIO.readFile("src\\resources\\input.txt");
+	    rwIO.readAndWrite("input.txt", "output.txt");
     }
 }
--- week4/src/com/kranaatinheitinkomppania/ReadAndWriteIO.java
+++ week4/src/com/kranaatinheitinkomppania/ReadAndWriteIO.java
@@ -2,17 +2,47 @@
 Author: Jani Heinikoski | 0541122
 Date: 18.1.2020
 Header: CT60A2411_07.01.2020 | Olio-ohjelmointi | WEEK 3
-Version: 4.1.0
+Version: 4.2.2
  */
 package com.kranaatinheitinkomppania;
 import java.io.*;
-import java.nio.charset.StandardCharsets;
+
 
 
 public class ReadAndWriteIO {
 
     public ReadAndWriteIO() {
         // Class constructor
+    }
+
+    public void readAndWrite(String inputFilePath, String outputFilePath) {
+        // Reads a .txt -file from filePath and prints to stdout.
+        if (inputFilePath.trim().isEmpty() || outputFilePath.trim().isEmpty()) {
+            System.out.println("Filepath can not be empty!");
+            return;
+        }
+        String tempLine;
+        // Check for file missing and IO exceptions.
+        try {
+            BufferedReader input = new BufferedReader(new FileReader(inputFilePath));
+            BufferedWriter output = new BufferedWriter(new FileWriter(outputFilePath));
+            // Read while line isn't null.
+            while ((tempLine = input.readLine()) != null) {
+                output.write(tempLine);
+                output.newLine();
+            }
+            input.close(); output.close();
+        }
+        // Raises when file is missing.
+        catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+            System.out.println("Input: " + inputFilePath + "Output: " + outputFilePath);
+        }
+        // Raises when error in reading from the file.
+        catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Filepath: " + inputFilePath);
+        }
     }
 
     public void readFile(String filePath) {
@@ -24,7 +54,7 @@ public class ReadAndWriteIO {
         String tempLine;
         // Check for file missing and IO exceptions.
         try {
-            BufferedReader input = new BufferedReader(new FileReader(filePath, StandardCharsets.UTF_8));
+            BufferedReader input = new BufferedReader(new FileReader(filePath));
             // Read while line isn't null.
             while ((tempLine = input.readLine()) != null) {
                 System.out.println(tempLine);
