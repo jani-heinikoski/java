5ba7b6b6ec26b4d516613fb9984057106aa14ac1
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-01-18 20:14:48 +0200

    ver_4.5.0
    Completely rewritten ReadAndWriteIO and Main for task 4.5

--- week4/src/com/kranaatinheitinkomppania/Main.java
+++ week4/src/com/kranaatinheitinkomppania/Main.java
@@ -2,15 +2,21 @@
 Author: Jani Heinikoski | 0541122
 Date: 18.1.2020
 Header: CT60A2411_07.01.2020 | Olio-ohjelmointi | WEEK 3
-Version: 4.4.0
+Version: 4.5.0
  */
 package com.kranaatinheitinkomppania;
+import java.nio.file.NoSuchFileException;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
-	    ReadAndWriteIO rwIO = new ReadAndWriteIO(30, 't');
-	    //rwIO.readAndWrite("input.txt", "output.txt");
-        rwIO.readAndWrite("src\\resources\\input.txt", "src\\resources\\output.txt");
+        try {
+            ReadAndWriteIO rwIO = new ReadAndWriteIO("src\\resources\\input.zip");
+            rwIO.readFileFromZip("input.txt");
+        } catch (NoSuchFileException e) {
+            e.printStackTrace();
+        }
     }
 }
--- week4/src/com/kranaatinheitinkomppania/ReadAndWriteIO.java
+++ week4/src/com/kranaatinheitinkomppania/ReadAndWriteIO.java
@@ -2,127 +2,79 @@
 Author: Jani Heinikoski | 0541122
 Date: 18.1.2020
 Header: CT60A2411_07.01.2020 | Olio-ohjelmointi | WEEK 3
-Version: 4.4.0
+Version: 4.5.0
  */
 package com.kranaatinheitinkomppania;
 import java.io.*;
+import java.nio.file.NoSuchFileException;
+import java.util.Scanner;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 
 
 public class ReadAndWriteIO {
 
-    private int lineLen;
-    private char c;
+    private ZipFile zipFile;
+    private String zipFilePath;
 
-    public ReadAndWriteIO(int lineLength, char linesWithChar) {
+    public ReadAndWriteIO(String zipFilePath) throws NoSuchFileException {
         // Class constructor
-        this.lineLen = lineLength;
-        this.c = linesWithChar;
+        setZipFile(zipFilePath);
     }
 
-    public ReadAndWriteIO() {
-        // Class constructor
-        this.lineLen = -1;
-    }
-
-    private boolean isAcceptLine (String line) {
-        // Checks if line is accepted by given rules.
-        // Requires bool containsChar() to be true.
-        // Used with void readAndWrite.
-        if (containsChar(line)) { // Always true if no char specified in constructor.
-            if (this.lineLen <= 0 || (line.length() < this.lineLen && !line.isBlank())) {
-                return true;
-            }
+    private void setZipFile(String zipFilePath) throws NoSuchFileException {
+        // Check if filepath to the .zip file is empty.
+        if (!zipFilePath.trim().isEmpty()) {
+            this.zipFilePath = zipFilePath;
+        } else {
+            throw new NoSuchFileException("Filepath can't be empty!");
         }
-        return false;
-    }
-
-    private boolean containsChar(String line) {
-        // Checks if line contains a given character.
-        if (this.c == '\u0000') { // If c has not been given, always true.
-            return true;
-        }
-        for (int i = 0; i < line.length(); i++) {
-            if (line.charAt(i) == this.c) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public void readAndWrite(String inputFilePath, String outputFilePath) {
-        // Reads a .txt -file from filePath and prints to stdout.
-        // Check if filepaths are empty.
-        if (inputFilePath.trim().isEmpty() || outputFilePath.trim().isEmpty()) {
-            System.out.println("Filepath can not be empty!");
-            return;
-        }
-        String tempLine;
-        // Check for file missing and IO exceptions.
+        // Try to instantiate ZipFile based on given path.
         try {
-            BufferedReader input = new BufferedReader(new FileReader(inputFilePath));
-            BufferedWriter output = new BufferedWriter(new FileWriter(outputFilePath));
-            // Read while line isn't null.
-            while ((tempLine = input.readLine()) != null) {
-                if (isAcceptLine(tempLine)) { // Default constructor => always true.
-                    output.write(tempLine);
-                    output.newLine();
-                }
-            }
-            input.close(); output.close();
-        }
-        // Raises when file is missing.
-        catch (FileNotFoundException ex) {
-            ex.printStackTrace();
-            System.out.println("Input: " + inputFilePath + "Output: " + outputFilePath);
-        }
-        // Raises when error in reading from the file.
-        catch (IOException e) {
+            this.zipFile = new ZipFile(this.zipFilePath);
+        } catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Filepath: " + inputFilePath);
         }
     }
 
-    public void readFile(String filePath) {
-        // Reads a .txt -file from filePath and prints to stdout.
-        if (filePath.trim().isEmpty()) {
-            System.out.println("Filepath can not be empty!");
+    public void readFileFromZip(String fileName) {
+        // Reads file to stdout from
+        ZipEntry file;
+        // Try to get the wanted file from the zip.
+        try {
+            file = this.zipFile.getEntry(fileName);
+        } catch (Exception e) {
+            e.printStackTrace();
             return;
         }
         String tempLine;
         // Check for file missing and IO exceptions.
         try {
-            BufferedReader input = new BufferedReader(new FileReader(filePath));
+            Scanner input = new Scanner(this.zipFile.getInputStream(file));
             // Read while line isn't null.
-            while ((tempLine = input.readLine()) != null) {
+            while (input.hasNextLine()) {
+                tempLine = input.nextLine();
                 System.out.println(tempLine);
             }
         }
         // Raises when file is missing.
         catch (FileNotFoundException ex) {
             ex.printStackTrace();
-            System.out.println("File not found, path: " + filePath);
+            System.out.println("File not found, path: " + this.zipFilePath);
         }
         // Raises when error in reading from the file.
         catch (IOException e) {
             e.printStackTrace();
-            System.out.println("Filepath: " + filePath);
+            System.out.println("Filepath: " + this.zipFilePath);
         }
     }
 
-    /**
-     * Set \u0000 to disable search (default value)
-     * @param c
-     */
-    public void setCharacterToSearch(char c) {
-        this.c = c;
+    public String getZipFilePath() {
+        return zipFilePath;
     }
 
-    /**
-     * Length < 0 disables length limit (default -1)
-     * @param lineLen
-     */
-    public void setLineLen(int lineLen) {
-        this.lineLen = lineLen;
+    public ZipFile getZipFile() {
+        return zipFile;
     }
 }
