c9ca91112161536655939d98c462a56940108c6b
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-02-28 20:10:21 +0200

    hotfix logger

--- week8/app/src/main/java/com/example/bottledispenser/MainActivity.java
+++ week8/app/src/main/java/com/example/bottledispenser/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
             bd.addMoney();
             refresh();
         } else {
-            log("LOGGER: Not enough money!");
+            log("Not enough money!");
         }
 
     }
