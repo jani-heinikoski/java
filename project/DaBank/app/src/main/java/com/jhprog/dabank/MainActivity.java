a2f714e60d31be268f7117095aca1135dc015032
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-04-03 14:52:30 +0300

    Project Started
    - ViewBinding enabled

---
+++ project/DaBank/app/src/main/java/com/jhprog/dabank/MainActivity.java
@@ -0,0 +1,38 @@
+/*
+* Author: Jani Olavi Heinikoski
+* Date: 03.04.2020
+* Version: 1.0
+* Sources:
+* https://material.io/
+* https://developer.android.com/training/data-storage/sqlite
+* */
+package com.jhprog.dabank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+import com.jhprog.dabank.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Disables screen rotation (locked to portrait mode)
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+    private void initButtons() {
+
+    }
+}
