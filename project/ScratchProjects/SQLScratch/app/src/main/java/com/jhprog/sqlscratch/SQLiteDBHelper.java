a97c0474ffdc524abf24d59a8e24fbc39754e628
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-04-03 15:47:16 +0300

    Update SQLiteDBHelper.java

--- project/ScratchProjects/SQLScratch/app/src/main/java/com/jhprog/sqlscratch/SQLiteDBHelper.java
+++ project/ScratchProjects/SQLScratch/app/src/main/java/com/jhprog/sqlscratch/SQLiteDBHelper.java
@@ -7,6 +7,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 public class SQLiteDBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "testi";
+    public static final int DATABASE_VERSION = 1;
 
     public SQLiteDBHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
