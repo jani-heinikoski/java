a2b0d5672c1944788c4ec2c2953c1d97b1baa3e6
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2020-04-14 11:04:56 +0300

    fragment_add_account update

--- project/DaBank/app/src/main/java/com/jhprog/dabank/admin/AddAccountFragment.java
+++ project/DaBank/app/src/main/java/com/jhprog/dabank/admin/AddAccountFragment.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.jhprog.dabank.R;
+import com.jhprog.dabank.data.Account;
 import com.jhprog.dabank.data.Customer;
 import com.jhprog.dabank.data.DataManager;
 import com.jhprog.dabank.databinding.FragmentAddAccountBinding;
@@ -30,7 +32,10 @@ public class AddAccountFragment extends Fragment {
     
     private FragmentAddAccountBinding binding;
     private ArrayList<Customer> customers;
-    private ArrayAdapter<Customer> arrayAdapter;
+    private ArrayAdapter<Customer> customerArrayAdapter;
+    private ArrayAdapter<String> accountTypeArrayAdapter;
+    private int accountType;
+    private final String[] accountTypes = {"Current Account", "Savings Account", "Fixed-term Account"};
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,11 +54,41 @@ public class AddAccountFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        arrayAdapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()).getApplicationContext(),
+        initSpinners();
+    }
+
+    private void initSpinners() {
+        // Array adapter for the customer selection Spinner
+        customerArrayAdapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()).getApplicationContext(),
                 R.layout.fragment_new_payment_spinner_item,
                 customers);
-        arrayAdapter.setDropDownViewResource(R.layout.fragment_new_payment_spinner_dropdown_item);
-        binding.fragmentAddAccountSpinnerCustomers.setAdapter(arrayAdapter);
+        customerArrayAdapter.setDropDownViewResource(R.layout.fragment_new_payment_spinner_dropdown_item);
+        binding.fragmentAddAccountSpinnerCustomers.setAdapter(customerArrayAdapter);
+        // Array adapter for the Account type Spinner
+        accountTypeArrayAdapter = new ArrayAdapter<>(Objects.requireNonNull(getActivity()).getApplicationContext(),
+                R.layout.fragment_new_payment_spinner_item,
+                accountTypes);
+        accountTypeArrayAdapter.setDropDownViewResource(R.layout.fragment_new_payment_spinner_dropdown_item);
+        binding.fragmentAddAccountSpinnerType.setAdapter(accountTypeArrayAdapter);
+        // Called when user chooses an account type from the spinner
+        binding.fragmentAddAccountSpinnerType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selected = view.toString();
+                if (selected.equals(accountTypes[0])) {
+                    accountType = Account.TYPE_CURRENT;
+                } else if (selected.equals(accountTypes[1])) {
+                    accountType = Account.TYPE_SAVING;
+                } else if (selected.equals(accountTypes[2])) {
+                    accountType = Account.TYPE_FIXED_TERM;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                accountType = 0;
+            }
+        });
     }
 
     private void initButtons() {
@@ -66,7 +101,7 @@ public class AddAccountFragment extends Fragment {
                     return;
                 } else {
                     customers = DataManager.getInstance().getCustomersByName(AdminActivity.getB_id(), cust_name);
-                    arrayAdapter.notifyDataSetChanged();
+                    customerArrayAdapter.notifyDataSetChanged();
                 }
             }
         });
